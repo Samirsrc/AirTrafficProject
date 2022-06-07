6e12c6a19ba14bc38946fd4e23eb95280d19051c
Samir <41807740+Samirsrc@users.noreply.github.com>
2022-06-08 01:51:45 +0200

    Update AllocationDeCreneauxDeDecollageModele2.java

--- src/AllocationDeCreneauxDeDecollageModele2.java
+++ src/AllocationDeCreneauxDeDecollageModele2.java
@@ -4,6 +4,8 @@ import org.chocosolver.solver.constraints.Constraint;
 import org.chocosolver.solver.variables.IntVar;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AllocationDeCreneauxDeDecollageModele2 {
     // j'utilise cette variable pour calculer les distances pour mon petit exemple dans un plan cartésien
@@ -11,61 +13,56 @@ public class AllocationDeCreneauxDeDecollageModele2 {
 
     public static void SolveProblem(ArrayList<Vol> vols) {
 
-        //nombre de dij
-        int dij_count = ((vols.size() - 1) * vols.size()) / 2;  // Par exemple si on a 4 vols, la comparaison va se faire entre les vols (1 et 2) (1 et 3) (1 et 4) (2 et 3) (2 et 4) (3 et 4) donc ça nous fait 6 dij
         //compteur pour le remplissage du tableau dij
         int dij_index = 0;
         //retard max
         int retardMax = 8;
 
         //appeler la methode getBornes qui va nous retourner les intervalles
-        ArrayList<ArrayList<BornesLbUb>> leslbubPourChaqueDijGotten = getBornes(vols);
+        HashMap<String, ArrayList<BornesLbUb>> lesintervallesGotten = getBornes(vols);
+
         //modele
-        /*  CSP {
-            X = { retardi,  i ∈ [1, n] } ∪ {dij ,(i, j) ∈ [1, n] , i < j }
-            D =  [0,retardmax] ∪ [-retardmax,retardmax]
-            C = {(dij = retardj − retardi) ∧ (dij !∈ Cij), (i, j) ∈ [1, n], i < j}
-            }
-            (dij !∈ Cij) ⇔ dij =! Tki − Tlj  */
         Model model = new Model("allocation de creneaux de decollage");
+
         //variables
         //retardi,  i ∈ [1, n]
         IntVar[] retards = model.intVarArray("retard avion", vols.size(), 0, retardMax);
-        //dij ,(i, j) ∈ [1, n] , i < j
-        IntVar[] dij = model.intVarArray("dij", dij_count, -retardMax, retardMax);
+        //dij
+        IntVar[] dij = new IntVar[0];
 
         //contraintes
-        //contrainte : dij = retardj − retardi
         dij_index = 0;
-        for (int i = 0; i <= vols.size(); i++) {
-            for (int j = i + 1; j < vols.size(); j++) {
-                dij[dij_index].eq(retards[j].sub(retards[i])).post();
-                dij_index++;
-            }
-        }
 
-        //contrainte : dij !∈ Cij
-        // on applique la contrainte à tout les dij
-        for (int i = 0; i < dij_count; i++) {
+        //contrainte : dij !∈ aux intervalles
+        //on applique la contrainte à tout les dij
+        for(Map.Entry m:lesintervallesGotten.entrySet()){
+            dij = new IntVar[dij_index+1];
+            dij[dij_index] = model.intVar(m.getKey().toString(),-retardMax,retardMax);
+            String splitdij[] = m.getKey().toString().split("D");
+            String splitdij2[] = splitdij[1].split("#");
+            dij[dij_index].eq(retards[Integer.parseInt(splitdij2[1])].sub(retards[Integer.parseInt(splitdij2[0])])).post();
+            dij_index++;
 
-            //Cette liste de contraintes on y met
             ArrayList<Constraint> cs = new ArrayList<Constraint>();
-            for (int j = 0; j < leslbubPourChaqueDijGotten.get(i).size(); j++) {
-                Constraint c1 = model.arithm(dij[i], "<", leslbubPourChaqueDijGotten.get(i).get(j).lb);
-                Constraint c2 = model.arithm(dij[i], ">", leslbubPourChaqueDijGotten.get(i).get(j).ub);
+            for (int j = 0; j < lesintervallesGotten.get(m.getKey()).size(); j++) {
+                Constraint c1 = model.arithm(dij[dij_index-1], "<", lesintervallesGotten.get(m.getKey()).get(j).lb);
+                Constraint c2 = model.arithm(dij[dij_index-1], ">", lesintervallesGotten.get(m.getKey()).get(j).ub);
                 cs.add(model.or(c1, c2));
             }
             if (cs.size() > 1) {
                 Constraint cand = model.and(cs.get(0), cs.get(1));
                 for (int j = 2; j < cs.size(); j++) {
+
                     cand = model.and(cand, cs.get(j));
+
                 }
                 cand.post();
             } else {
-                if (cs.size()==0){
+                if (cs.size() == 0) {
 
-                }else {
-                cs.get(0).post();}
+                } else {
+                    cs.get(0).post();
+                }
             }
 
         }
@@ -80,10 +77,11 @@ public class AllocationDeCreneauxDeDecollageModele2 {
     }
 
     //methode pour obtenir les intervalles
-    public static ArrayList getBornes(ArrayList<Vol> vols) {
+    public static HashMap<String, ArrayList<BornesLbUb>> getBornes(ArrayList<Vol> vols) {
 
         //Les intervalles des conflits pour chaque dij
-        ArrayList<ArrayList<BornesLbUb>> leslbubPourChaqueDij = new ArrayList<ArrayList<BornesLbUb>>();
+        HashMap<String, ArrayList<BornesLbUb>> lesintervalles = new HashMap<String, ArrayList<BornesLbUb>>();
+
         // distance separation en NM
         float distanceSeparation = 20;
         if (geodesique == false) distanceSeparation = 5;
@@ -91,41 +89,48 @@ public class AllocationDeCreneauxDeDecollageModele2 {
         int dij_index = 0;
         int kk = 0;
         boolean firstk = false;
+        boolean dij_bool = false;
 
         //pour chaque dij
         for (int i = 0; i < vols.size(); i++) {
             for (int j = i + 1; j < vols.size(); j++) {
-                leslbubPourChaqueDij.add(new ArrayList());
+                //       leslbubPourChaqueDij.add(new ArrayList());
                 //parcourir toutes les paires
                 for (int k = 0; k < vols.get(i).trajectoire4D.points.size(); k++) {
                     for (int l = 0; l < vols.get(j).trajectoire4D.points.size(); l++) {
 
                         // si la distance entre les deux points < 5 mille nautique alors il a conflit donc il faut que l'instant de passage soit différent
                         if (obtenirDistance(vols.get(i).trajectoire4D.points.get(k).longitude, vols.get(i).trajectoire4D.points.get(k).latitude, vols.get(j).trajectoire4D.points.get(l).longitude, vols.get(j).trajectoire4D.points.get(l).latitude) < distanceSeparation) {
-                            //    System.out.println("Nous sommes en conflit : avion " + (i + 1) + " point " + (k) + " avec avion " + (j + 1) + " point " + (l));
-                            System.out.println("Nous sommes en conflit : avion " + (i + 1) + " point " + (k) + " " + vols.get(i).trajectoire4D.points.get(k).longitude + " " + vols.get(i).trajectoire4D.points.get(k).latitude + " avec avion " + (j + 1) + " point " + (l) + " " + vols.get(j).trajectoire4D.points.get(l).longitude + " " + vols.get(j).trajectoire4D.points.get(l).latitude);
+
+                         System.out.println("vol("+i+") point("+k+") (lon: "+ vols.get(i).trajectoire4D.points.get(k).longitude + " | lat: "+ vols.get(i).trajectoire4D.points.get(k).latitude + " | instant: "+ vols.get(i).trajectoire4D.points.get(k).instantDePassage + " )   en conflit avec   vol("+j+") point("+l+") (lon: "+ vols.get(j).trajectoire4D.points.get(l).longitude + " | lat: "+ vols.get(j).trajectoire4D.points.get(l).latitude + " | instant: "+ vols.get(j).trajectoire4D.points.get(l).instantDePassage + " )    || Tik-Tjl = "+(vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage));
+
+
+                            if (dij_bool == false) {
+                                lesintervalles.put(("D" + i + "#" + j), new ArrayList<BornesLbUb>());
+                                dij_bool = true;
+                                dij_index++;
+                            }
 
                             // premier k, premier point de conflit
                             if (firstk == false) {
                                 kk = k;
                                 firstk = true;
-                                //initialisation de LB et UB à 0
-                                leslbubPourChaqueDij.get(dij_index).add(new BornesLbUb(0, 0));
-
+                                //initialisation de LB et UB
+                                lesintervalles.get("D"+i+"#"+j).add(new BornesLbUb((vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage),(vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage)));
                             }
                             // passage au conflit suivant
                             if (k > kk + 1) {
                                 conflit_index++;
                                 //ajout de l'intervalle du conflit suivant au meme dij
-                                leslbubPourChaqueDij.get(dij_index).add(new BornesLbUb(0, 0));
+                                lesintervalles.get("D"+i+"#"+j).add(new BornesLbUb((vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage),(vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage)));
                             }
                             kk = k;
 
-                            if (leslbubPourChaqueDij.get(dij_index).get(conflit_index).getLb() > (vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage)) {
-                                leslbubPourChaqueDij.get(dij_index).get(conflit_index).setLb((vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage));
+                            if ( lesintervalles.get("D"+i+"#"+j).get(conflit_index).getLb() > (vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage)) {
+                                lesintervalles.get("D"+i+"#"+j).get(conflit_index).setLb((vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage));
                             }
-                            if (leslbubPourChaqueDij.get(dij_index).get(conflit_index).getUb() < (vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage)) {
-                                leslbubPourChaqueDij.get(dij_index).get(conflit_index).setUb((vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage));
+                            if (lesintervalles.get("D"+i+"#"+j).get(conflit_index).getUb() < (vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage)) {
+                                lesintervalles.get("D"+i+"#"+j).get(conflit_index).setUb((vols.get(i).trajectoire4D.points.get(k).instantDePassage - vols.get(j).trajectoire4D.points.get(l).instantDePassage));
                             }
 
                         } else {
@@ -136,13 +141,13 @@ public class AllocationDeCreneauxDeDecollageModele2 {
                 dij_index++;
                 firstk = false;
                 conflit_index = 0;
-                //        System.out.println(" dijindex" + dij_index);
+                dij_bool = false;
             }
         }
-        System.out.println(leslbubPourChaqueDij);
-        System.out.println(leslbubPourChaqueDij.size());
+        System.out.println(lesintervalles);
+        System.out.println(lesintervalles.size());
 
-        return leslbubPourChaqueDij;
+        return lesintervalles;
     }
 
 
@@ -150,6 +155,7 @@ public class AllocationDeCreneauxDeDecollageModele2 {
         if (geodesique == false) {
             double xDiff = aX - bX;
             double yDiff = aY - bY;
+
             return (double) Math.sqrt(xDiff * xDiff + yDiff * yDiff);
 
         } else {
